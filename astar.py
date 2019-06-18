302b88436a55b08a8e03c068a6b2c2dbfcc1311d
Mariam Joan <royalton@gmail.com>
2019-06-17 22:28:14 -0700

    Update astar.py

--- astar.py
+++ astar.py
@@ -5,22 +5,28 @@ import heapq
 from collections import defaultdict
 import math 
 
+import heapq
+from collections import defaultdict
+import math 
+
 def shortest_path(edges, nodes, start, goal):
     closedset = set()
     
     openset = list()  
-    heapq.heappush(openset, (start, 0)) 
+    
+    # heapq will store in tuple(heuristic_estimated_cost, current_node, distance_cost)
+    heapq.heappush(openset, (0, start, 0)) 
         
     came_from = defaultdict()
     
-    g = [math.inf] * (len(M.intersections) + 1)  # dictionary?
+    g = [math.inf] * (len(M.intersections) + 1)  
     g[start] = 0
     
     f = defaultdict()
     f[start] = calculate_distance(M, start, goal)
      
     while len(openset) > 0:
-        (g_cost, current) = heapq.heappop(openset) 
+        (g_cost, current, distance_cost) = heapq.heappop(openset) 
         
         if current == goal:
             return a_star_search_path(came_from, current)
@@ -33,31 +39,30 @@ def shortest_path(edges, nodes, start, goal):
                 continue
 
             distance_cost = g_cost + calculate_distance(M, current, neighbor)
+            
             heuristic = calculate_distance(M, neighbor, goal)
             f[neighbor] = g[neighbor] + heuristic
             
-            if neighbor in openset and distance_cost <= g[neighbor]:
-                heapq.heappush(openset, (neighbor, f[neighbor]))
-                
-            else:
-                g[neighbor] = distance_cost
-                came_from[neighbor] = current   
+            heapq.heappush(openset, (f[neighbor], neighbor, distance_cost))
+            
+            g[neighbor] = distance_cost
+            came_from[neighbor] = current   
                   
-    return False
+    return "No path found"
 
 def a_star_search_path(came_from, current):
     path = []
-    while current in came_from.keys():
+    keys = [key for key in came_from.keys()]
+    while current in keys:
         current = came_from[current]
         path.append(current)
     return path
-
 
 def calculate_distance(M, intersection1, intersection2):
     current_x_y = M.intersections[intersection1]
     neighbor_x_y = M.intersections[intersection2]
     return math.sqrt(sum( [ (a - b) ** 2 for a, b in zip(current_x_y, neighbor_x_y) ] ))
-    
+
 
 # Citations: 
 # 1: https://www.w3resource.com/python-exercises/python-basic-exercise-40.php
